99bfdf86d329ebf0904a1607ef1f09eab8431878
Andrea Hernandez <andreajhl29@gmail.com>
2024-03-05 08:09:09 -0300

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export const SELECT_ALL = 'all';
+
+export const checkboxOptions = [
+  {
+    name: SELECT_ALL,
+    label: 'Seleccionar todos',
+  },
+  {
+    name: 'india',
+    label: 'India',
+  },
+  {
+    name: 'usa',
+    label: 'USA',
+  },
+  {
+    name: 'france',
+    label: 'Francia',
+  },
+];
